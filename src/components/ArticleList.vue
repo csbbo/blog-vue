44dcd8d757ffc9981fa1c66dd1ac70ead02f6c0d
chen <chenshaobo272@gmail.com>
2019-02-28 18:53:46 +0800

    v1.0

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -22,9 +22,9 @@
         </div>
         <div v-else class="not-content">
             <div class="introdution">
-                “ 很遗憾! 什么也没找到,送你一首 ‘空空如也’ ”
+                “ 很遗憾! 什么也没找到,送你一首 ‘泡沫’ ”
             </div>
-            <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=1336170083&auto=0&height=66"></iframe>
+            <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=26113988&auto=1&height=66"></iframe>
         </div>
     </div>
 </template>
--- src/global.js
+++ src/global.js
@@ -1,3 +1,4 @@
 export default {
-    host : 'http://127.0.0.1:8000'
+    // host : 'http://127.0.0.1:8000',
+    host : 'http://background.shaobo.fun'
 }
